73268b4376ff48eed217f966f5d436e124ba8e97
Erwan KESSLER <45232488+erwan-kessler@users.noreply.github.com>
2021-01-18 17:26:39 +0100

    v_m

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ print(matx.shape)
 image_shape = matx[0][0].shape
 d = image_shape[0] * image_shape[1]
 # plt.imshow(matx[0][0])
-# plt.show()
+# 
 print(image_shape)
 matx = np.asarray([m[0].flatten() for m in matx])  # remove parenthesis
 mat = np.transpose(matx)
@@ -61,44 +61,77 @@ print(eigen_face.shape)
 # show an eigenface
 plt.figure(1)
 plt.imshow(eigen_face[5,].real.reshape(image_shape))
-plt.show()
+
 
 # show an eigenface over image
 plt.figure(2)
 plt.imshow(matx[0].reshape(image_shape))
-plt.show()
+
 
 plt.figure(3)
 plt.imshow(matx[0].reshape(image_shape))
 plt.imshow(eigen_face[0,].real.reshape(image_shape))
-plt.show()
+
 
 for i in range(2):
     plt.figure(4 + i * 3)
-    img = eigen_face[i,].real*255 + matx[i]
+    img = eigen_face[i,].real * 255 + matx[i]
     plt.imshow(img.reshape(image_shape))
     plt.title("Addition of both")
 
     plt.figure(4 + i * 3 + 1)
-    img = matx[i] - eigen_face[i,].real*255
+    img = matx[i] - eigen_face[i,].real * 255
     plt.imshow(img.reshape(image_shape))
     plt.title("Difference of both")
 
-    plt.show()
+    
     plt.figure(4 + i * 3 + 2)
     plt.imshow(matx[i].reshape(image_shape))
     plt.title("face")
-    plt.show()
+    
     plt.figure(4 + i * 3 + 3)
     plt.imshow(eigen_face[i].real.reshape(image_shape))
     plt.title("eigen face")
-    plt.show()
+    
 
-# import random
+# Question 7
+import random
 
-# m=random.randint(1,d)
-# eig_temp=[(v.real,i) for i,v in enumerate(eig_value)]
-# eig_temp.sort(key=lambda x:x[0],reverse=True)
-# eig_temp=eig_temp[:m]
-# eig_value_extracted=[eig_value[i] for v,i in eig_temp]
-# print(len(eig_value_extracted),m)
+m = random.randint(1, d)  # [1,d)
+
+
+def extract_m_largest(m):
+    # listing all the eigen value, with their respecting indexes
+    eig_temp = [(v.real, i) for i, v in enumerate(eig_value)]
+    # sorting the eigen values
+    eig_temp.sort(key=lambda x: x[0], reverse=True)
+    # m first largest eigen values
+    eig_temp = eig_temp[:m]
+    # extract phiM eigenvectors with the m largest eigenvalues
+    eig_vector_extracted = [eig_vector[i] for _, i in eig_temp]
+    print(len(eig_vector_extracted), m)
+    return eig_vector_extracted
+
+
+# Question 3.3.1
+from sklearn.model_selection import train_test_split
+
+data, labels = matx, range(len(matx))
+
+data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.5, random_state=42) \
+
+def v(_m):
+    return sum(eig_value[:_m].real) / sum(eig_value.real)
+
+
+v_m = [v(_m) for _m in range(d)]
+
+plt.figure(15)
+plt.plot(range(d), v_m)
+
+
+plt.figure(16)
+plt.plot(range(500), v_m[:500])
+
+
+plt.show()
