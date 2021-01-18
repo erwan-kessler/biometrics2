149dbf50a7569a91283ebe46b67f6974cb4d1156
Erwan KESSLER <e.t.kessler@student.utwente.nl>
2021-01-18 16:59:38 +0100

    Show addition of both faces

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.linalg as la
+
 loaded = scipy.io.loadmat('FaceData.mat')
-mat: np.ndarray = loaded['FaceData']
-shape = mat.shape
-mat = mat.flatten()
-print(mat.shape)
-image_shape=mat[0][0].shape
-d=image_shape[0]*image_shape[1]
-plt.imshow(mat[0][0])
-plt.show()
+matx: np.ndarray = loaded['FaceData']
+
+# create a matrix 'mat' with dimensions dxn with d = pxq and n = number of pictures
+shape = matx.shape
+matx = matx.flatten()
+print(matx.shape)
+image_shape = matx[0][0].shape
+d = image_shape[0] * image_shape[1]
+# plt.imshow(matx[0][0])
+# plt.show()
 print(image_shape)
-mat = np.asarray([m[0].flatten() for m in mat])  # remove parenthesis
-print(mat[0].shape)
+matx = np.asarray([m[0].flatten() for m in matx])  # remove parenthesis
+mat = np.transpose(matx)
+print(mat.shape)
 
 
+# calculate the mean
 def compute_mean(mat):
     m = 0
     for col in mat:
@@ -23,39 +29,76 @@ def compute_mean(mat):
 
 
 theta_0 = compute_mean(mat)
+print("theta", theta_0)
 
+# Create a training matrix
 x_0 = np.asarray([m - theta_0 for m in mat])
-n = x_0.shape[0]
+print("xo shape", x_0.shape)
 
+# n is number of figures
+n = x_0.shape[1]
+print(n)
 
-def get_covariance_matrix(x_0, n):
-    m = []
-    for col in x_0:
-        m.append(np.dot(col, col.transpose()))
-    return np.asarray(m) * (1 / (n - 1))
-
-
-get_covariance_matrix(x_0, n)
-cov = 1 / (n - 1) * np.dot(x_0.transpose(), x_0)
+# Create covariance matrix with dimensions dxd (2576 x 2576)
+cov = (1 / (n - 1)) * np.dot(x_0, x_0.transpose())
+# print(cov)
 print(cov.shape)
-eig_value, eig_vector = np.linalg.eig(cov)
 
-for i in range(0):
-    img=eig_vector[i].real+mat[i]
+# Compute eigenvectors and eigenfaces
+eig = la.eig(cov)
+# Print eigenvalues
+eig_value = eig[0]
+# print(eig_value)
+# print eigenvectors
+eig_vector = eig[1]
+print(eig_vector)
+print(eig_vector.shape)
+
+# reshape vectors
+eigen_face = eig_vector
+print(eigen_face.shape)
+
+# show an eigenface
+plt.figure(1)
+plt.imshow(eigen_face[5,].real.reshape(image_shape))
+plt.show()
+
+# show an eigenface over image
+plt.figure(2)
+plt.imshow(matx[0].reshape(image_shape))
+plt.show()
+
+plt.figure(3)
+plt.imshow(matx[0].reshape(image_shape))
+plt.imshow(eigen_face[0,].real.reshape(image_shape))
+plt.show()
+
+for i in range(2):
+    plt.figure(4 + i * 3)
+    img = eigen_face[i,].real*255 + matx[i]
     plt.imshow(img.reshape(image_shape))
+    plt.title("Addition of both")
+
+    plt.figure(4 + i * 3 + 1)
+    img = matx[i] - eigen_face[i,].real*255
+    plt.imshow(img.reshape(image_shape))
+    plt.title("Difference of both")
+
     plt.show()
-    plt.imshow(mat[i].reshape(image_shape))
+    plt.figure(4 + i * 3 + 2)
+    plt.imshow(matx[i].reshape(image_shape))
+    plt.title("face")
     plt.show()
-    plt.imshow(eig_vector[i].real.reshape(image_shape))
+    plt.figure(4 + i * 3 + 3)
+    plt.imshow(eigen_face[i].real.reshape(image_shape))
+    plt.title("eigen face")
     plt.show()
 
+# import random
 
-
-import random
-
-m=random.randint(1,d)
-eig_temp=[(v.real,i) for i,v in enumerate(eig_value)]
-eig_temp.sort(key=lambda x:x[0],reverse=True)
-eig_temp=eig_temp[:m]
-eig_value_extracted=[eig_value[i] for v,i in eig_temp]
-print(len(eig_value_extracted),m)
+# m=random.randint(1,d)
+# eig_temp=[(v.real,i) for i,v in enumerate(eig_value)]
+# eig_temp.sort(key=lambda x:x[0],reverse=True)
+# eig_temp=eig_temp[:m]
+# eig_value_extracted=[eig_value[i] for v,i in eig_temp]
+# print(len(eig_value_extracted),m)
